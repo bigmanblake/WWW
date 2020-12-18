1972db67651c5713aafdd437ee80239fed9377d0
bigmanblake <47894996+bigmanblake@users.noreply.github.com>
2020-12-18 11:23:48 +0000

    why are we here just to suffer
    trying to fix the code only makes it more broken
    fuck git
    fuck coding
    and fuck websites i want to make a game or at least learn about coding in the context of gaming who thought that making a web page would be a good idea in a GAME DESIGN course.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// image, sourcex, sourcey, sourcewidth, sourceheight,
+// destx, desty, destwidth, destheight
+ctx.drawImage(playerImage, 0, 0,
+ playerImage.width / 2, playerImage.height / 2,
+ x, 10, playerImage.height / 2, playerImage.height / 2);
